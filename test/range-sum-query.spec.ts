4eb401537dbb995da5682fe961390663d415f2f5
mesfin.mebrate <mesfin.mebrate@kaluza.com>
2026-08-22 21:28:38 +0100

    Range Sum Query - Immutable: https://leetcode.com/problems/range-sum-query-immutable/submissions/2116597096/

--- test/range-sum-query.spec.ts
+++ test/range-sum-query.spec.ts
@@ -1,8 +1,8 @@
 import { describe, expect, it } from 'vitest';
 import { NumArray } from '~/range-sum-query';
 
-describe('NumArray', () => {
-  it('sumRange should work for example 1', () => {
+describe('NumArray.sumRange', () => {
+  it('should work for example 1', () => {
     const numArray: NumArray = new NumArray([-2, 0, 3, -5, 2, -1]);
 
     expect(numArray.sumRange(0, 2)).toEqual(1);
@@ -10,7 +10,7 @@ describe('NumArray', () => {
     expect(numArray.sumRange(0, 5)).toEqual(-3);
   });
 
-  it('sumRange should work for example 2', () => {
+  it('should work for example 2', () => {
     const numArray: NumArray = new NumArray([1]);
 
     expect(numArray.sumRange(0, 0)).toEqual(1);
